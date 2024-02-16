9be98786c62b22b84583facf41f8355435341b65
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2024-02-16 17:42:39 +0000

    Updated main.c function with task switching structure and STUB blocks

--- src/data.c
+++ src/data.c
@@ -1,3 +1,10 @@
 #include "data.h"
 
-/* TODO File Stub, To Implement */
+void data_load(programData_t * data, char const * fileName) {
+    data->run = 1;
+    /* TODO STUB, Implement function later */
+}
+
+void data_save(programData_t data, char const * fileName) {
+    /* TODO STUB, Implement function later */
+}
--- src/main.c
+++ src/main.c
@@ -5,16 +5,33 @@
 */
 
 #include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "terminal_gui/terminal_f.h"
 
 #include "data.h"
 
+/** The global static variable used to transfer data from a signal handler to the program. 
+ * Initialized to a NULL pointer, optimally to be set to the address of a variable used in the program.
+*/
+static short * signalRun = NULL;
+
+/** Function to handle received SIGINT signal to allow for graceful program termination */
+void signalHandler(int sigID);
+
 
 int main() {
+
+    /* Attaching a signal handler for a graceful exit on receiving SIGINT */
+    struct sigaction signalControl = {0};
+    signalControl.sa_handler = signalHandler;
+    sigaction(SIGINT, &signalControl, NULL);
 
     /* Task-independent data declaration and initialization */
     programData_t data = {0};
     data_load(&data, ".game_data");
+    /* Attaching the data.run program data variable to the static global signal processing variable */
+    signalRun = &(data.run);
 
     /* Terminal GUI initialization */
     screenSave();
@@ -34,12 +51,14 @@ int main() {
             /* TODO STUB Implement */
             break;
 
+            /* Graceful exit program procedure */
             case TASK_EXIT:
                 data.run = 0;
             break;
 
+            /* Default behavior (if no correct nextTask set) - exit program gracefully */
             default:
-            /* TODO STUB Implement */
+                nextTask = TASK_EXIT;
         }
     }
 
@@ -53,4 +72,17 @@ int main() {
 
     return 0;
 
+}
+
+
+/* === Other function implementations === */
+
+void signalHandler(int sigID) {
+    if(signalRun != NULL) {
+        /* Setting the attached run variable to 0 to terminate program gracefully */
+        *signalRun = 0;
+    } else {
+        /* Hard exit if no signal handling run variable attached (pointer is NULL) */
+        exit(1);
+    }
 }
